4afa6cd557c794d5c9d87e0dbf24ad07c7a93627
subramanyamdvss <subramanyamdvss@gmail.com>
2017-02-20 19:41:32 +0530

    Update loadData.py

--- vqa-access/loadData.py
+++ vqa-access/loadData.py
@@ -1,4 +1,4 @@
-dataDir='/users/Datasets/VQA/data'
+dataDir='/media/surya/OS/DISC 1/VQA/data'
 taskType='OpenEnded'
 dataType='mscoco' # 'mscoco' for real and 'abstract_v002' for abstract
 dataSubType='train2014'
